3cc8423b9b245a6990adf72689eb598f9f452670
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-11-09 19:25:01 +0100

    add hashed password

--- php/register.php
+++ php/register.php
@@ -6,8 +6,9 @@ $nome = $connection->real_escape_string( $_POST['nome']);
 $cognome = $connection->real_escape_string( $_POST['cognome']);
 $email = $connection->real_escape_string( $_POST['email']);
 $password = $connection->real_escape_string( $_POST['password']);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$password')";
+$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$hashed_password')";
 if ($connection->query($sql) === true){
   echo "Registrazione effettuata con successo";
 }else{
