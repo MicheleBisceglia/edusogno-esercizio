df4e6c5ed064a08f305f19685d09e8b32097c29f
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-11-09 12:24:20 +0100

    index page creation

--- index.php
+++ index.php
@@ -10,7 +10,49 @@
 </head>
 
 <body>
-    <h1 class="title">hello world</h1>
-</body>
+  <!-- Header -->
+  <header></header>
+  <!-- /Header -->
 
+   <!-- Main -->
+  <div class="container">
+    <!-- Titolo -->
+    <h1 class="title">
+      Crea il tuo account
+    </h1>
+    <!-- /Titolo -->
+    
+    <!-- Register form -->
+    <div class="form-container">
+      <form action="">
+        <div class="form-item">
+          <label for="nome">Inserisci il tuo nome</label> <br>
+          <input type="text" name="nome" placeholder="Mario">
+        </div>
+
+        <div class="form-item">
+          <label for="cognome">Inserisci il tuo cognome</label> <br>
+          <input type="text" name="cognome" placeholder="Rossi">
+        </div>
+
+        <div class="form-item">
+          <label for="email">Inserisci la tua e-mail</label> <br>
+          <input type="email" id="email" placeholder="name@example.com">
+        </div>
+
+        <div class="form-item">
+          <label for="password">Inserisci la tua password</label> <br>
+          <input type="password" name="password" placeholder="Scrivila qui">
+        </div>
+      </form>
+
+      <div class="form-btn">
+        Registrati
+      </div>
+    <!-- /Register form -->
+
+    </div>
+  </div>
+  <!-- /Main -->
+</body>
 </html>
