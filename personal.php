da6d88ef6fd4324320bd17d8b62bca84ac1c193e
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-11-09 12:54:34 +0100

    personal page creation

---
+++ personal.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edusogno</title>
+    <link rel="stylesheet" href="../assets/styles/style.css">
+</head>
+<body>
+  <!-- Header -->
+  <header></header>
+  <!-- /Header -->
+
+  <!-- Main -->
+  <div class="container">
+    <h1 class="title">
+      Ciao Nome ecco i tuoi eventi
+    </h1>
+
+    <!-- events -->
+    <div class="event-container">
+      <div class="event">
+        <h3>Nome evento</h3>
+        <p>15-03-03</p>
+        <div class="event-btn">
+          JOIN
+        </div> 
+      </div>
+      <div class="event">
+        <h3>Nome evento</h3>
+        <p>15-03-03</p>
+        <div class="event-btn">
+          JOIN
+        </div> 
+      </div>
+      <div class="event">
+        <h3>Nome evento</h3>
+        <p>15-03-03</p>
+        <div class="event-btn">
+          JOIN
+        </div> 
+      </div>
+    </div>
+    <!-- /events -->
+  </div>
+  <!-- /Main -->
+</body>
+</html>
